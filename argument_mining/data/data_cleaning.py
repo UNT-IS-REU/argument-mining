5a6f14171d330f1c4a3bcc753b84e5aef62661f5
Sanjeev Penupala <sanjeev.penupala@gmail.com>
2020-08-03 17:07:50 -0500

    Added Invalid Sentences DataFrame

--- argument_mining/data/data_cleaning.py
+++ argument_mining/data/data_cleaning.py
@@ -17,15 +17,14 @@ import pandas as pd
 ########
 
 
-def get_best_and_conflict_labels(stage_num: str, team_num: str, team_path: str) -> None:
+def get_bic_labels(stage_num: str, team_num: str, team_path: str) -> None:
     """
-    - Takes all the CSV files in a team's folder and gets the majority selected label for every sentence and any sentences
-    that has conflicted labels (meaning there is not definitive majority label)
-    - Puts best label and best label ID into a DataFrame, along with the sentence
-    - Puts conflict label and conflict label text into a DataFrame, along with the sentence
+    - BIC = Best, Invalid, and Conflict Labels
+    - Takes all the CSV files in a team's folder and gets BIC labels and put them into their respective DataFrames
     - Convert DataFrames into a CSV file
-        - "best_labels_filename" format: "s<stage_number>_t<team_number>_best_labels.csv"
-        - "conflict_labels_filename" format: "s<stage_number>_t<team_number>_conflict_labels.csv"
+        - "best_labels_filename" format:        "s<stage_number>_t<team_number>_best_labels.csv"
+        - "conflict_labels_filename" format:    "s<stage_number>_t<team_number>_conflict_labels.csv"
+        - "invalid_labels_filename" format:     "s<stage_number>_t<team_number>_invalid_labels.csv"
 
     :param stage_num:   Stage Number In Dataset
     :param team_num:    Team Number In Stage
@@ -44,14 +43,13 @@ def get_best_and_conflict_labels(stage_num: str, team_num: str, team_path: str) 
         7: 'Invalid sentences'
     }
 
-    data = OrderedDict()                        # Keeps Sentences (Keys) in Order of Input
-    best_df = pd.DataFrame()                    # DataFrame for Best Labels
-    conflict_df = pd.DataFrame()                # DataFrame for Best Labels
-    csv_files = sorted(os.listdir(team_path))   # Get List of All CSV Files in a Team
+    data = OrderedDict()                                                                # Keeps Sentences (Keys) in Order of Input
+    best_df, conflict_df, invalid_df = pd.DataFrame(), pd.DataFrame(), pd.DataFrame()   # DataFrames for BIC Labels
+    csv_files = sorted(os.listdir(team_path))                                           # Get List of All CSV Files in a Team
 
     for csv_file in csv_files:
         # Skip Over "best_labels.csv" or "conflict_labels.csv" Files, If Found
-        if "best_labels" in csv_file or "conflict_labels" in csv_file:
+        if "best_labels" in csv_file or "conflict_labels" in csv_file or "invalid_labels" in csv_file:
             continue
 
         csv_file_path = os.path.join(team_path, csv_file)
@@ -66,38 +64,47 @@ def get_best_and_conflict_labels(stage_num: str, team_num: str, team_path: str) 
             else:
                 data[text] = [label_id]         # Otherwise, Add New Sentence To Dict, With List of Labels
 
-    # Find The Best (Majority) Label For Every Sentence
+    # Find The BIC Label For Every Sentence
     for text, label_ids in data.items():
         counts = Counter(label_ids)
-        two_most_freq = counts.most_common(2)
+        two_most_freq = counts.most_common(2)   # Format: [(label_id, frequency_count), (...), ...]
 
-        # Check If The Top 2 Labels Are The Same Count
+        # Check If The Top 2 Labels Are The Same Count (This Indicates That There Is No Majority Label)
         if len(two_most_freq) > 1 and two_most_freq[0][1] == two_most_freq[1][1]:
-            label_texts = ", ".join([labels[label_id] for label_id in label_ids])
-            conflict_df = conflict_df.append([[text, label_texts]], ignore_index=True)  # Append To Conflict DataFrame As Row
+            label_texts = ", ".join(labels[label_id] for label_id in label_ids)     # Convert List of Labels To A String
+            conflict_df = conflict_df.append([[text, label_texts]], ignore_index=True)
+        # Check If The Majority Label is a Invalid Sentence
+        elif (best_label_id := two_most_freq[0][0]) == 7:
+            invalid_df = invalid_df.append([[text, best_label_id, 7]], ignore_index=True)
+        # Append The Best Label To The Best DataFrame (Do Not Append Other Sentences)
         else:
-            best_label_id = two_most_freq[0][0]
-            if best_label_id != 7:
-                best_df = best_df.append([[text, best_label_id, labels[best_label_id]]], ignore_index=True)  # Append To Best DataFrame As Row
+            if best_label_id != 6:
+                best_df = best_df.append([[text, best_label_id, labels[best_label_id]]], ignore_index=True)
 
-    best_df.columns = ['text', 'label_id', 'label_text']    # Set Column Names Of Best Labels DataFrame
-    best_df.sort_values(by=['label_id'], inplace=True)      # Sort Sentences By Label ID
-    conflict_df.columns = ['text', 'label_text']            # Set Column Names Of The Conflict Labels DataFrame
+    best_df.columns = ['text', 'label_id', 'label_text']        # Set Column Names Of Best Labels DataFrame
+    best_df.sort_values(by=['label_id'], inplace=True)          # Sort Sentences By Label ID, For Best DataFrame
+    invalid_df.columns = ['text', 'label_id', 'label_text']     # Set Column Names Of Invalid Labels DataFrame
+    conflict_df.columns = ['text', 'label_text']                # Set Column Names Of The Conflict Labels DataFrame
 
-    # Make Filename For Best Labels and Conflict Labels CSV File
+    # Make Filename For BIC Label CSV Files
     best_labels_filename = "".join(['s', stage_num,
                                     '_t', team_num,
                                     '_best_labels.csv'])
+    invalid_labels_filename = "".join(['s', stage_num,
+                                       '_t', team_num,
+                                       '_invalid_labels.csv'])
     conflict_labels_filename = "".join(['s', stage_num,
                                         '_t', team_num,
                                         '_conflict_labels.csv'])
 
     # Get Full Path To Best Labels and Conflict Labels CSV Files
     best_labels_path = os.path.join(team_path, best_labels_filename)
+    invalid_labels_path = os.path.join(team_path, invalid_labels_filename)
     conflict_labels_path = os.path.join(team_path, conflict_labels_filename)
 
     # Convert Best Labels and Conflict Labels DataFrames Into CSV File
     best_df.to_csv(best_labels_path, index=False, encoding='ISO-8859-1')
+    invalid_df.to_csv(invalid_labels_path, index=False, encoding='ISO-8859-1')
     conflict_df.to_csv(conflict_labels_path, index=False, encoding='ISO-8859-1')
 
 
@@ -121,4 +128,4 @@ def clean(dataset_path: str) -> None:
         for team in teams:
             team_path = os.path.join(stage_path, team)
             team_num = team[-1]
-            get_best_and_conflict_labels(stage_num, team_num, team_path)  # Compile and Consolidate Best Labels Among Team Members
+            get_bic_labels(stage_num, team_num, team_path)  # Compile and Consolidate Best Labels Among Team Members
